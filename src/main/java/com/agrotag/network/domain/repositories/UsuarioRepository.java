100162c14cfeabeb0d048ff2c59b6a302f212374
EricHafemann <eric070gabriel@gmail.com>
2026-04-02 11:14:22 -0300

    feat(repository): cadiiconaUsuarioRepository interface com CRUD básico

---
+++ src/main/java/com/agrotag/network/domain/repositories/UsuarioRepository.java
@@ -0,0 +1,26 @@
+package com.agrotag.network.domain.repositories;
+
+
+
+import com.agrotag.network.domain.entities.Usuario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioRepository {
+
+    Optional<Usuario> findById(Long id);
+    Usuario save(Usuario user);
+    void deleteById(Long id);
+    boolean existsById(Long id);
+
+    List<Usuario> findAll();
+
+    void delete(Usuario user);
+
+    Optional<Usuario> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    List<Usuario> findAllWithTaskCount();
+}
