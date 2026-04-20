9b965833d897970b8b832f156d4323710a013855
Eric Hafemann <eric070gabriel@gmail.com>
2026-04-20 10:01:13 -0300

    feat(controller): adiciona controller de autentificação.

---
+++ src/main/java/com/agrotag/network/application/controllers/AuthController.java
@@ -0,0 +1,15 @@
+package com.agrotag.network.application.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "Acesse /oauth2/authorization/google para autenticar";
+    }
+}
